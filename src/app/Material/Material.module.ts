c44c5a6e27142261e50870e2c436e57fcf2ce183
Douglas Moraes <douglas.moraes@jfpart.com.br>
2023-07-29 21:37:57 -0300

    incluindo sidenav

---
+++ src/app/Material/Material.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+
+
+
+
+@NgModule({
+  exports: [
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule
+  ],
+  declarations: []
+})
+export class MaterialModule { }
